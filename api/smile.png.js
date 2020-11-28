6887adcce2a64473219640c1b4a90ddbd841dd6a
notepraiya <notepraiya@gmail.com>
2020-11-28 20:32:41 +0700

    add ua

--- api/smile.png.js
+++ api/smile.png.js
@@ -3,8 +3,6 @@ module.exports = (req, res) => {
     body: req.body,
     query: req.query,
     cookies: req.cookies,
-    ipa: req.connection.remoteAddress,
-    ipb: req.headers['x-forwarded-for'],
     ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
   })
 }
---
+++ api/test101.js
@@ -0,0 +1,14 @@
+var parser = require('ua-parser-js');
+
+module.exports = (req, res) => {
+  var ua = parser(req.headers['user-agent']);
+  res.json({
+    ...ua,
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+    ipr: req.connection.remoteAddress,
+    ipx: req.headers['x-forwarded-for'],
+    ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+  })
+}
