0531bdc0c371f1fd18b66a7385b157d7c167df54
notepraiya <notepraiya@gmail.com>
2020-11-28 19:32:33 +0700

    ip

--- api/getImage.js
+++ api/getImage.js
@@ -3,5 +3,8 @@ module.exports = (req, res) => {
     body: req.body,
     query: req.query,
     cookies: req.cookies,
+    ipa: req.connection.remoteAddress,
+    ipb: req.headers['x-forwarded-for'],
+    ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
   })
 }
